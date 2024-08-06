c8c96a1a3730cd7b5009d60595cf2f5a32d77922
laetitiabeles <laetitiabeles9@gmail.com>
2024-08-06 13:46:23 +0200

    fixing welcome section

--- src/components/WelcomeSection.js
+++ src/components/WelcomeSection.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import styles from '../styles/Welcome.module.css';
+import WelcomeImage from '../assets/welcome.png';
 
 const Welcome = () => {
   return (
-    <section className={styles.welcomeSection}>
-      <div className={styles.left}></div>
-      <div className={styles.right}>
-        <h2>Bienvenue</h2>
-        <p>Informations nécessaires ici...</p>
+    <section id="welcome" className={styles.welcomeSection}>
+      <div className={styles.contentContainer}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.title}>Hi!</h1>
+          <h1 className={styles.title}>I'm Laëtitia Bélès</h1>
+          <h2 className={styles.subtitle}>Software engineering student looking for an apprenticeship</h2>
+          <h3 className={styles.description}>I am a passionate and curious programming student always looking for new skills to acquire and new challenges to take on.</h3>
+        </div>
+        <div className={styles.imageContainer}>
+          <img src={WelcomeImage} alt="Bienvenue" className={styles.WelcomeImage} />
+        </div>
       </div>
     </section>
   );
