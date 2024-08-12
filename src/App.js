804a3a508eff0a6a7dda5b9c4e6790137b601e98
laetitiabeles <laetitiabeles9@gmail.com>
2024-08-12 18:08:43 +0200

    add App files

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
 import DotNav from './components/DotNav';
+import Contact from './components/Contact';
 import './App.css';
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
         <Skills />
         <Projects />
         <DotNav />
+        <Contact />
       </div>
       <Footer />
     </div>
